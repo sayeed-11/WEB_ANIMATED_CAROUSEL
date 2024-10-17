1b53d570a15e1e9c4e962f3d8c1bcf6a732bb02b
sayeed-11 <110775831+sayeedar11@users.noreply.github.com>
2024-10-17 18:26:00 +0530

    animated_carousel
    carousel

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import App from './App';
+import Carousel from './Components/Carousel/Carousel';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Carousel />
+  </React.StrictMode>
+);
+
